92b0dba189a4cf01da7aa939f16cf3c181eac9c7
Ayman <Ayman.hachkal@gmail.com>
2025-09-18 19:25:44 +0100

    Added some Instructions to Enum up to OR A, N8. And changed Register and Flag Register to a dataclass
    Changed Register and Flag Register to dataclass to make accessing the
    flags through the F register easier.

--- src/CPU.py
+++ src/CPU.py
@@ -1,3 +1,5 @@
+from re import A
+from Registers import Register, FlagsRegister
 import numpy as np
 from enum import Enum
 
@@ -11,26 +13,127 @@ class ArithmeticTarget(Enum):
     F = 6
     H = 7
     L = 8
+    AF = 9
+    BC = 10
+    DE = 11
+    HL = 12
+    SP = 13
 
 # Insturctions
 class Instruction(Enum):
-    ADD = ArithmeticTarget
+    #Arithmetic 
+    ADCHLR8 = ArithmeticTarget.HL
+    ADCR8 = ArithmeticTarget
+    ADDR8 = ArithmeticTarget
+    ADDHLR8 = ArithmeticTarget.HL
+    ADDSP = ArithmeticTarget.SP
+    ADDN8 = 1 
+    ADDHLR16 = ArithmeticTarget
+    ADDHLSP = ArithmeticTarget.SP
+    ADDSPE8 = 2
+
+    #Logic
+    ANDR8 = ArithmeticTarget
+    ANDHL = ArithmeticTarget.HL
+    ANDN8 = 3
+
+    BITR8 = ArithmeticTarget
+    BITHL = ArithmeticTarget.HL
+
+    CALLN16 = 4
+    CALLCCN16 = 5
+
+    CCF = 6
+
+    CPR8 = ArithmeticTarget
+    CPHL = ArithmeticTarget.HL
+    CPN8 = 7
+    CPL = ArithmeticTarget.A
+
+    DAA = 8
+    
+    DECR8 = ArithmeticTarget
+    DECHL = ArithmeticTarget.HL
+    DECR16 = ArithmeticTarget
+    DECSP = 9
+
+    DI = 10
+    EL = 11
+    HALT = 12
+
+    INCR8 = ArithmeticTarget
+    INCHL = ArithmeticTarget.HL
+    INCSP = 13
+    
+    JPN16 = 14
+    JPCCN16 = 15
+    JPHL = ArithmeticTarget.HL
+
+    JRN16 = 16
+    JRCCN16 = 17 
+
+    LDR8 = ArithmeticTarget
+    LDN8 = 18
+    LDN16 = 19
+    LDHLR8 = ArithmeticTarget
+    LDHLN8 = 20
+    LDR8HL = ArithmeticTarget.HL
+    LDR16A = ArithmeticTarget
+    LDN16A = 21
+    LDHN16A = 22
+    LDHCA = ArithmeticTarget.C
+    LDR16 = ArithmeticTarget 
+    LDHN16 = 23
+    LDHC = ArithmeticTarget.C
+    LDHLIA = ArithmeticTarget.HL
+    LDHLDA = ArithmeticTarget.HL
+    LDAHLD = ArithmeticTarget.HL
+    LDAHLI = ArithmeticTarget.HL
+    LDSPN16 = 24
+    LDN16SP = 25
+    LDHLSPE8 = ArithmeticTarget.HL
+    LDSPHL = ArithmeticTarget.HL
+    NOP = 26
+    ORR8 = ArithmeticTarget
+    ORHL = ArithmeticTarget.HL
+    ORN8 = 27
 
 
 
-class CPU:
+
+
+class CPU(Register):
     def __init__(self):
-        pass
-
+        super().__init__()
 
     def execute(self, instruction: Instruction):
         # Case statement to check which instruction is being used 
         match instruction:
-            case Instruction.ADD:
+            case Instruction.ADDR8:
                 # Case statement to check which register ADD insturction is using
-                match instruction.ADD.value:
+                match instruction.ADDR8.value:
                     case ArithmeticTarget.C:
-                        pass
+                        value = self.c
+                        new_value = self.add(value)
+                        self.a = new_value
                     case _:
                         pass
+
+
+    def add(self, value: np.uint8) -> np.uint8: 
+        value = self.a + value
+        overflow_check = self.check_overflow_uint8(int(self.a), int(value))
+        self.f.zero = True if self.a == 0 else False
+        self.f.subtract = False
+        self.f.carry = overflow_check
+        self.f.half_carry = True if (((self.a & 0xF) + (value & 0xF)) > 0xF) else False
+        return value
+
+    def check_overflow_uint8(self, register_value: int, value: int) -> bool:
+        if register_value + value > 255:
+            return True
+        else:
+            return False 
+
+
 
--- src/Registers.py
+++ src/Registers.py
@@ -1,85 +1,23 @@
 import numpy as np
+from dataclasses import dataclass
 
-class Register:
-    def __init__(self) -> None:
-        self.register = {
-            'a': np.uint8(),
-            'b': np.uint8(),
-            'c': np.uint8(),
-            'd': np.uint8(),
-            'e': np.uint8(),
-            'f': np.uint8(),
-            'h': np.uint8(),
-            'l': np.uint8(),
-        }
-    
-    #af registers 
-    def get_af(self) -> np.uint16:
-        af = np.uint16(
-            (self.register['a'] << 8) | self.register['f']
-        )
-        return af
-
-    def set_af(self, value: np.uint16):
-        self.register['a'] = np.uint8((value & 0xff00) >> 8)    #0xff00 == 1111111100000000  
-        self.register['f'] = np.uint8(value & 0xff)             #0xff   == 0000000011111111
-
-
-    #bc registers
-    def get_bc(self) -> np.uint16:
-        bc = np.uint16(
-            (self.register['b'] << 8) | self.register['c']
-        )
-        return bc
-
-    def set_bc(self, value: np.uint16):
-        self.register['b'] = np.uint8((value & 0xff00) >> 8)
-        self.register['c'] = np.uint8(value & 0xff)
-
-
-    #de registers
-    def get_de(self) -> np.uint16:
-        de = np.uint16(
-            (self.register['d'] << 8) | self.register['e']
-        )
-        return de
-
-    def set_de(self, value: np.uint16):
-        self.register['d'] = np.uint8((value & 0xff00) >> 8)
-        self.register['e'] = np.uint8(value & 0xff)
-
-
-    #hl registers
-    def get_hl(self) -> np.uint16:
-        hl = np.uint16(
-            (self.register['h'] << 8) | self.register['l']
-        )
-        return hl 
-
-    def set_hl(self, value: np.uint16):
-        self.register['h'] = np.uint8((value & 0xff00) >> 8)
-        self.register['l'] = np.uint8(value & 0xff)
-
-
+@dataclass
 class FlagsRegister: 
-    def __init__(self) -> None:
-        self.flag_register : dict[str, bool]= {
-            "zero"      : bool(),
-            "subtract"  : bool(),
-            "half_carry": bool(), 
-            "carry"     : bool()
-        }
-        self.ZERO_FLAG_BYTE_POSITION : int = 7
-        self.SUBTRACT_FLAG_BYTE_POSITION : int = 6
-        self.HALF_CARRY_FLAG_BYTE_POSITION : int = 5
-        self.CARRY_FLAG_BYTE_POSITION : int = 4
+    zero      : bool = False
+    subtract  : bool = False
+    half_carry: bool = False
+    carry     : bool = False
+    ZERO_FLAG_BYTE_POSITION : int = 7
+    SUBTRACT_FLAG_BYTE_POSITION : int = 6
+    HALF_CARRY_FLAG_BYTE_POSITION : int = 5
+    CARRY_FLAG_BYTE_POSITION : int = 4
 
     def get_flags(self) -> np.uint8: 
         return np.uint8(
-            (np.uint8(1 if self.flag_register['zero'] else 0) << self.ZERO_FLAG_BYTE_POSITION) | 
-            (np.uint8(1 if self.flag_register['subtract'] else 0) << self.SUBTRACT_FLAG_BYTE_POSITION) |
-            (np.uint8(1 if self.flag_register['half_carry'] else 0) << self.HALF_CARRY_FLAG_BYTE_POSITION) |
-            (np.uint8(1 if self.flag_register['carry'] else 0) << self.CARRY_FLAG_BYTE_POSITION)
+            (np.uint8(1 if self.zero else 0) << self.ZERO_FLAG_BYTE_POSITION) | 
+            (np.uint8(1 if self.subtract else 0) << self.SUBTRACT_FLAG_BYTE_POSITION) |
+            (np.uint8(1 if self.half_carry else 0) << self.HALF_CARRY_FLAG_BYTE_POSITION) |
+            (np.uint8(1 if self.carry else 0) << self.CARRY_FLAG_BYTE_POSITION)
         )
 
     def set_flags(self, byte: np.uint8): 
@@ -87,8 +25,68 @@ class FlagsRegister:
         subtract : bool = bool(((byte >> self.SUBTRACT_FLAG_BYTE_POSITION) & 1))
         half_carry : bool = bool(((byte >> self.HALF_CARRY_FLAG_BYTE_POSITION) & 1))
         carry : bool = bool(((byte >> self.CARRY_FLAG_BYTE_POSITION) & 1))
-        self.flag_register['zero'] = zero
-        self.flag_register['subtract'] = subtract 
-        self.flag_register['half_carry'] = half_carry 
-        self.flag_register['carry'] = carry 
+        self.zero = zero
+        self.subtract = subtract 
+        self.half_carry = half_carry 
+        self.carry = carry 
+
+@dataclass
+class Register:
+    a: np.uint8 = np.uint8(0)
+    b: np.uint8 = np.uint8(0) 
+    c: np.uint8 = np.uint8(0)
+    d: np.uint8 = np.uint8(0)
+    e: np.uint8 = np.uint8(0)
+    f = FlagsRegister()
+    h: np.uint8 = np.uint8(0)
+    l: np.uint8 = np.uint8(0)
+    sp : np.uint16 = np.uint16(0)
+    
+    #af registers 
+    def get_af(self) -> np.uint16:
+        af = np.uint16(
+            (self.a << 8) | self.f.get_flags()
+        )
+        return af
+
+    def set_af(self, value: np.uint16):
+        self.a = np.uint8((value & 0xff00) >> 8)    #0xff00 == 1111111100000000  
+        self.f.set_flags(np.uint8(value & 0xff))             #0xff   == 0000000011111111
+
+
+    #bc registers
+    def get_bc(self) -> np.uint16:
+        bc = np.uint16(
+            (self.b << 8) | self.c
+        )
+        return bc
+
+    def set_bc(self, value: np.uint16):
+        self.b = np.uint8((value & 0xff00) >> 8)
+        self.c = np.uint8(value & 0xff)
+
+
+    #de registers
+    def get_de(self) -> np.uint16:
+        de = np.uint16(
+            (self.d << 8) | self.e
+        )
+        return de
+
+    def set_de(self, value: np.uint16):
+        self.d = np.uint8((value & 0xff00) >> 8)
+        self.e = np.uint8(value & 0xff)
+
+
+    #hl registers
+    def get_hl(self) -> np.uint16:
+        hl = np.uint16(
+            (self.h << 8) | self.l
+        )
+        return hl 
+
+    def set_hl(self, value: np.uint16):
+        self.h = np.uint8((value & 0xff00) >> 8)
+        self.l = np.uint8(value & 0xff)
+
 
